d2ca9fc908da3085586a6d6ee58bff0caad675d1
afinado <afinado@a1.net>
2018-09-10 18:19:06 +0200

    JSF HelloWorld

---
+++ jsfdemo/JSFDemo/src/main/java/com/concretepage/UserLogin.java
@@ -0,0 +1,46 @@
+package com.concretepage;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "userLogin", eager = true)
+@RequestScoped
+public class UserLogin {
+	private String message = "Enter username and password.";
+	private String username;
+	private String password;
+
+	public String login() {
+		if ("concretepage".equalsIgnoreCase(username) && "concretepage".equalsIgnoreCase(password)) {
+			message = "Successfully logged-in.";
+			return "success";
+		} else {
+			message = "Wrong credentials.";
+			return "login";
+		}
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
